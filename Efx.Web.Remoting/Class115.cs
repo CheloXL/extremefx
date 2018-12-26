4b4d2b7935ee1bd214eb3b0ea23c833bc87a14ec
Marcelo Volmaro <mvolmaro@gmail.com>
2018-12-26 20:55:56 -0300

    No iba...

--- Efx.Web.Remoting/Class115.cs
+++
@@ -1,9 +0,0 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Class115
-// Assembly: Efx.Web.Remoting, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 11D5333A-8A85-4DAC-8B61-C8CAFAF3E798
-// Assembly location: D:\Code\2DS\Aplicaciones\a\dll\.NET 4.0\Efx.Web.Remoting.dll
-
-internal static class Class115
-{
-}
